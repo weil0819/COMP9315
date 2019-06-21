02cca120cc6f3e84482cc440c0ead028282e3a20
Wesley Li <weil0819@gmail.com>
2019-06-21 17:31:34 +1000

    update prac

--- 19T2/Prac Exercises/p02/schema.sql
+++ 19T2/Prac Exercises/p02/schema.sql
@@ -1,3 +1,7 @@
+-- "pg_class" holds core information about tables
+-- "pg_attribute" contains information about attributes
+-- "pg_namespace" contains information about schemata
+
 create or replace function schema() returns setof text
 as $$
 declare
@@ -5,32 +9,32 @@ declare
         rel text := '';
         att text := '';
         out text := '';
-	len integer := 0;
+		len integer := 0;
 begin
 	for rec in
-		select relname,attname
+		select t.relname, a.attname     -- Name of the table, The column name
 		from   pg_class t, pg_attribute a, pg_namespace n
-		where  t.relkind='r'
-			and t.relnamespace = n.oid
-			and n.nspname = 'public'
-			and attrelid = t.oid
-			and attnum > 0
-		order by relname,attnum
+		where  t.relkind='r'			-- r=ordinary table
+			and t.relnamespace = n.oid  -- The OID of the namespace that contains this relation, pg_namespace.oid
+			and n.nspname = 'public'    -- Name of the namespace
+			and a.attrelid = t.oid      -- The table this column belongs to, pg_class.oid
+			and a.attnum > 0            -- The number of the column
+		order by t.relname, a.attnum         
 	loop
-		if (rec.relname <> rel) then
-			if (rel <> '') then
+		if (rec.relname <> rel) then    -- If table name is different from the former one
+			if (rel <> '') then         -- If relation name is non-null
 				out := rel || '(' || att || ')';
 				return next out;
 			end if;
-			rel := rec.relname;
+			rel := rec.relname;         -- relation name = table name 
 			att := '';
 			len := 0;
 		end if;
-		if (att <> '') then
-			att := att || ', ';
-			len := len + 2;
+		if (att <> '') then             -- If attribute-list is non-null
+			att := att || ', ';         -- Append attribute to attribute-list
+			len := len + 2;             -- string length + 2
 		end if;
-		if (len + length(rec.attname) > 70) then
+		if (len + length(rec.attname) > 70) then -- newline
 			att := att || E'\n        ';
 			len := 0;
 		end if;
--- 19T2/Prac Exercises/p02/schema1.sql
+++ 19T2/Prac Exercises/p02/schema1.sql
@@ -1,3 +1,5 @@
+-- atttypmod records type-specific data supplied at table creation time (for example, the maximum length of a varchar column)
+
 drop type if exists SchemaTuple cascade;
 create type SchemaTuple as ("table" text, "attributes" text);
 
@@ -6,13 +8,13 @@ as $$
 declare
         rec   record;
         rel   text := '';
-	attr  text := '';
+		attr  text := '';
         attrs text := '';
         out   SchemaTuple;
-	len   integer := 0;
+		len   integer := 0;
 begin
 	for rec in
-		select r.relname, a.attname, t.typname, a.atttypmod
+		select r.relname, a.attname, t.typname, a.atttypmod 
 		from   pg_class r
 			join pg_namespace n on (r.relnamespace = n.oid)
 			join pg_attribute a on (a.attrelid = r.oid)
@@ -24,8 +26,8 @@ begin
 	loop
 		if (rec.relname <> rel) then
 			if (rel <> '') then
-				out."table" := rel;
-				out.attributes := attrs;
+				out."table" := rel;			-- set out.table
+				out.attributes := attrs;    -- set out.attributes
 				return next out;
 			end if;
 			rel := rec.relname;
@@ -36,6 +38,7 @@ begin
 			attrs := attrs || ', ';
 			len := len + 2;
 		end if;
+		-- Add data type description
 		if (rec.typname = 'varchar') then
 			rec.typname := 'varchar('||(rec.atttypmod-4)||')';
 		elsif (rec.typname = 'bpchar') then
@@ -45,7 +48,7 @@ begin
 		elsif (rec.typname = 'float8') then
 			rec.typname := 'float';
 		end if;
-		attr := rec.attname||':'||rec.typname;
+		attr := rec.attname||':'||rec.typname;   -- attr includes attr name + data type
 		if (len + length(attr) > 50) then
 			attrs := attrs || E'\n';
 			len := 0;
